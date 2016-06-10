961e730a3b8bef9f690b82344e10adeb9d25d558
lovecn <shoushilvd1@sina.com>
2016-06-10 16:38:15 +0800

    Update js_array_unique.js

--- js_array_unique.js
+++ js_array_unique.js
@@ -49,3 +49,77 @@ console.log(arr.distinct(function (val) {
 }).map(function (val) {
     return val.val;
 })) [1,3]
+
+Array.prototype.distinct = function(){
+    var arr = this,
+        result = [],
+        i,
+        j,
+        len = arr.length;
+
+    for(i = 0; i < len; i++){
+        for(j = i + 1; j < len; j++){
+            if(arr[i] === arr[j]){
+                j = ++i;
+            }
+        }
+        result.push(arr[i]);
+    }
+    return result;
+}
+var arra = [1,2,3,4,4,1,1,2,1,1,1];
+arra.distinct();             //返回[3,4,2,1]
+Array.prototype.distinct = function (){
+    var arr = this,
+        i,
+        j,
+        len = arr.length;
+
+    for(i = 0; i < len; i++){
+        for(j = i + 1; j < len; j++){
+            if(arr[i] == arr[j]){
+                arr.splice(j,1);
+                len--; // 删除后，数组的长度也减1
+                j--;
+            }
+        }
+    }
+    return arr;
+};
+
+var a = [1,2,3,4,5,6,5,3,2,4,56,4,1,2,1,1,1,1,1,1,];
+var b = a.distinct();
+console.log(b.toString()); //1,2,3,4,5,6,56
+
+Array.prototype.distinct = function (){
+    var arr = this,
+        i,
+        obj = {},
+        result = [],
+        len = arr.length;
+
+    for(i = 0; i< arr.length; i++){
+        if(!obj[arr[i]]){    //如果能查找到，证明数组元素重复了
+            obj[arr[i]] = 1;
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+
+var a = [1,2,3,4,5,6,5,3,2,4,56,4,1,2,1,1,1,1,1,1,];
+var b = a.distinct();
+console.log(b.toString()); //1,2,3,4,5,6,56
+Array.prototype.distinct = function() {
+  var arr = this,
+      result = [],
+      len = arr.length,
+  for (var i = 0; i < len; i++) {
+    if (result.indexOf(arr[i]) === -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+var a = [1, 2, 3, 4, 2, 3, 54, 1, 2, 3, 2, 4, 5, 0];
+var b = a.distinct();    // [1, 2, 3, 4, 54, 5, 0]
