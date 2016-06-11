45abf2dbce7801b2b0220eaa87384ed77eeebbe9
lovecn <shoushilvd1@sina.com>
2016-06-11 18:36:59 +0800

    pdf转换成文本

---
+++ python_pdf.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+
+def readPDF(pdfFile):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+
+    process_pdf(rsrcmgr, device, pdfFile)
+    device.close()
+
+    content = retstr.getvalue()
+    retstr.close()
+    return content
+#如果PDF文件在你的电脑里，那就把urlopen返回的对象pdfFile替换成普通的open()文件对象
+pdfFile = urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf")
+outputString = readPDF(pdfFile)
+print(outputString)
+pdfFile.close()
